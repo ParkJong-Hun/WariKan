5bf2d7ba81f1284232dbc1897f0f030b5f91ebe7
ParkJong-Hun <thesn515@gmail.com>
2021-10-16 11:13:52 +0900

    2021/10/16 11:13 Generate App

---
+++ WariKan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WariKan
+//
+//  Created by 박종훈 on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
