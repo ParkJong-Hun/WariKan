1b64ee4523c636ec68d545e289e236f09cccd062
ParkJong-Hun <thesn515@gmail.com>
2021-10-31 15:42:19 +0900

    2021/10/31 15:41 割り勘分割基本設定'

--- WariKan/Controller/DutchPay/DutchPaySeperateController.swift
+++ WariKan/Controller/DutchPay/DutchPaySeperateController.swift
@@ -10,7 +10,29 @@ import UIKit
 class DutchPaySeperateController: UIViewController {
     var items:[Item] = []
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func clickedAddButton(_ sender: Any) {
+    }
+    @IBAction func clickedOkButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+}
+
+extension DutchPaySeperateController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
 }
