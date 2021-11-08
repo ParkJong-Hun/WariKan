bc0a4ba957f45ff0edafd900063b403ff10c6fd0
ParkJong-Hun <thesn515@gmail.com>
2021-11-08 20:21:42 +0900

    2021/11/08 20:11 割り勘を友達から選択する

--- WariKan/Controller/DutchPay/DutchPaySelectFriendController.swift
+++ WariKan/Controller/DutchPay/DutchPaySelectFriendController.swift
@@ -6,9 +6,53 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 class DutchPaySelectFriendController:UIViewController {
+    var friendName = [String]()
+    var selectedFriend = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let db = Firestore.firestore()
+        let auth = Auth.auth()
+        
+        db.collection("users").document(auth.currentUser!.uid).collection("friends")
+            .getDocuments(completion: { (documents,error) in
+                for document in documents!.documents {
+                    self.friendName.append(document["email"] as! String)
+                }
+            })
+        self.tableView.reloadData()
+    }
+    @IBAction func clickedAddButton(_ sender: Any) {
+        if !selectedFriend.isEmpty {
+            //TODO: 以前画面にデータ伝達
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    @IBOutlet weak var tableView: UITableView!
+}
+
+extension DutchPaySelectFriendController:UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        friendName.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = friendName[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.backgroundColor = .green
+        selectedFriend = friendName[indexPath.row]
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.backgroundColor = .none
     }
 }
